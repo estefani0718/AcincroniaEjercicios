70df63683573fc82a07c88d33c41327d881c0215
estefani0718 <eestefanisg@gmail.com>
2025-02-28 11:29:56 -0500

    inicio de taller

---
+++ js/ejercicio1.js
@@ -0,0 +1,11 @@
+//Escriba una función imprimirNumeros(desde, hasta) que genere un número cada segundo,
+// comenzando desde desde y terminando con hasta.
+// Haz dos variantes de la solución describiendo el paso a paso de cada una de ellas:
+// • Usando setInterval.
+// • Usando setTimeout anidado.
+
+function imprimirNumeros (desde, hasta) {
+  
+}
+let desde = parseInt(prompt("ingrese un numero :"));
+let hasta = parseInt(prompt("ingrese un numero :"));
